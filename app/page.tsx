ffd84bdddc3b5a151cabb8fa232a8c2cade3458f
Divyanshu <74353927+S1r1u511@users.noreply.github.com>
2025-10-31 13:48:52 +0530

    Project file

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+      <iframe
+        src="Playcharts.html"
+        title="CHARTS Game"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+      />
+    </main>
+  );
+}
